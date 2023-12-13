05b059dc5978130046dda744b2476314ea6c9f54
Leo Hammond <hammond.l@etoncollege.org.uk>
2023-12-13 09:59:36 +0000

    Initial Commit

---
+++ HackingAPI/HackingAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackingAPIApp.swift
+//  HackingAPI
+//
+//  Created by Leo Hammond on 13/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct HackingAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
