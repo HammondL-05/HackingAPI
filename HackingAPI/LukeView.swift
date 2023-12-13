234c6f4559aa81a8e018a5d3764ded60929ce293
Luke <vereker.l@etoncollege.org.uk>
2023-12-13 10:09:42 +0000

    leo is bad

---
+++ HackingAPI/LukeView.swift
@@ -0,0 +1,18 @@
+//
+//  LukeView.swift
+//  HackingAPI
+//
+//  Created by Vereker, Luke (PRKB) on 13/12/2023.
+//
+
+import SwiftUI
+
+struct LukeView: View {
+    var body: some View {
+        Text("Hello Luke")
+    }
+}
+
+#Preview {
+    LukeView()
+}
